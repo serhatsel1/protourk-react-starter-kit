e210d682c4420050d82d5b5fc8615f883615e973
Serhat Sel <selserhat01@gmail.com>
2024-01-03 19:57:09 +0300

    todo project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useEffect, useState } from "react";
-import Button from "./components/Button";
-import Tab from "./components/Tab";
-import Test from "./components/Test";
+import { useState } from "react";
 
 function App() {
-  // const [activeTab, setActiveTab] = useState(1);
-  const [show, setShow] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState();
 
+  const submitHandle = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("")
+
+  };
   return (
     <>
-      <button onClick={() => setShow((show) => !show)}>
-        {show ? "Gizle" : "Göster"}
-      </button>
-
-      {show ? <Test /> : ""}
-
-      {/* <button onClick={() => setActiveTab(2)} className="bg-gray-500 border">
-        Active tabı değiştir
-      </button>
-      <div style={{ padding: 20 }}>
-        <Tab activeTab={activeTab} setActiveTab={setActiveTab}>
-          <Tab.Panel title="ayarlar">1. tab</Tab.Panel>
-          <Tab.Panel title="hakkında">2. tab</Tab.Panel>
-          <Tab.Panel title="components">3. tab</Tab.Panel>
-        </Tab>
-      </div>
-      <Button>buton-children</Button>
-      <Button text="buton" variant="success" />
-      <Button text="buton" variant="danger" />
-      <Button text="buton" variant="warning" />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <h1 className="font-semibold text-2xl">TODO App </h1>
+      <form onSubmit={submitHandle}>
+        <input
+          className="border bg-gray-100"
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit"
+        disabled={!todo}>Ekle</button>
+      </form>
+      <ul>
+        {todos.map((todo, i) => (
+          <li key={i}>{todo}</li>
+        ))}
+      </ul>
     </>
   );
 }
